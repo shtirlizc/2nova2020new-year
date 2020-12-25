8b12567df2f1236d8d1d946d851b0d3979d00f66
Marat Sarvarov <shtirlizc@gmail.com>
2020-12-25 09:52:33 +0500

    style fixes

--- js/app.js
+++ js/app.js
@@ -12,6 +12,39 @@ const Loading = {
       ease: Linear.easeNone,
     });
   },
+  finish: function () {
+    $(".loading").fadeOut(500);
+    $("body").css("overflow", "auto");
+  },
+};
+
+const Wish = {
+  variants: [
+    "Ты не умрешь одиноким",
+    "Река сама вынесет трупы твоих врагов",
+    "Мир во всем Мире",
+    "Прочтешь все книги, которые планировал",
+  ],
+  init: function () {
+    const rand = Math.round(-0.5 + Math.random() * this.variants.length);
+    $(".cracker-message__inner").html(this.variants[rand]);
+
+    this.watch();
+  },
+  watch: function () {
+    $("#cracker").click(function () {
+      $(".cracker__label").fadeToggle(500);
+      $(this).toggleClass("active");
+    });
+  },
+};
+
+const Tree = {
+  watch: function () {
+    $("._tree .settings h1").click(function () {
+      $("._tree .settings").toggleClass("active");
+    });
+  },
 };
 
 const ShowScreen = {
@@ -32,18 +65,23 @@ const Music = {
   play: function () {
     $(".music__play").click(function () {
       const target = $(this).data("target");
-
-      console.log(target);
+      $(".music__play").removeClass("active");
+      $(this).addClass("active");
 
       $(".music__items audio").each(function () {
         $(this)[0].pause();
+        $(this)[0].currentTime = 0;
       });
       $(".music__items audio")[Number(target)].play();
     });
   },
 };
 
+Loading.init();
+
 window.addEventListener("load", (event) => {
-  Loading.init();
+  Loading.finish();
   Music.play();
+  Wish.init();
+  Tree.watch();
 });
