fb4443d0701767e2e716a60ba708d37ea797d7f2
Marat Sarvarov <shtirlizc@gmail.com>
2020-12-29 21:49:30 +0500

    letters data

--- js/app.js
+++ js/app.js
@@ -175,41 +175,144 @@ const State = {
 const Results = {
   data: [
     {
-      fistName: "Андрей",
-      lastName: "Елпаев",
-      from: "от Насти",
+      fistName: "Дима",
+      lastName: "Богаткин",
+      from: "от Марата",
       message:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, deserunt doloribus ducimus labore quisquam tempora velit! Deleniti laborum laudantium praesentium suscipit! Deserunt iure molestiae nemo ratione sunt? Asperiores, dolor, tempore.",
+        "Спасибо за то, что никогда не отказываешь в помощи. И счастливого Нового года!",
     },
     {
-      fistName: "Андрей",
-      lastName: "Елпаев",
-      from: "от Насти",
-      message:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, deserunt doloribus ducimus labore quisquam tempora velit! Deleniti laborum laudantium praesentium suscipit! Deserunt iure molestiae nemo ratione sunt? Asperiores, dolor, tempore.",
+      fistName: "Аня",
+      lastName: "Л.",
+      from: "от Лены А.",
+      message: "Пусть в новом году проекты будут приносить удовольствие!",
     },
     {
-      fistName: "Андрей",
-      lastName: "Елпаев",
-      from: "от Насти",
+      fistName: "Ксения",
+      lastName: "Ц.",
+      from: "от Яны У.",
       message:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, deserunt doloribus ducimus labore quisquam tempora velit! Deleniti laborum laudantium praesentium suscipit! Deserunt iure molestiae nemo ratione sunt? Asperiores, dolor, tempore.",
+        "Ты - моя вторая мама, желаю, чтобы в новом году тебя окружали такие же заботливые люди, пусть все задумки, даже самые невероятные дают свои первые плоды, и чтобы во всех нерабочих сферах, где ты успела попробовать себя, был заметен прогресс , и это только мотивировало бы тебя двигаться вперёд!  Спасибо за этот год! Ты обалденная, и ты знаешь это.",
     },
     {
-      fistName: "Андрей",
-      lastName: "Елпаев",
-      from: "от Насти",
+      fistName: "Даша",
+      lastName: "",
+      from: "от Яны У.",
       message:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, deserunt doloribus ducimus labore quisquam tempora velit! Deleniti laborum laudantium praesentium suscipit! Deserunt iure molestiae nemo ratione sunt? Asperiores, dolor, tempore.",
+        "Ты мой луч света в мраке вёрстки со своим уникальным юмором, который всегда вызывает улыбку умиления. Надеюсь, этот год не подорвал твои стремления и позитив, я надеюсь, что новый год тебе подарит больше приятных впечатлений! Почаще улыбайся, наше солнце! С Новым годом!",
     },
     {
-      fistName: "Андрей",
-      lastName: "Елпаев",
-      from: "от Насти",
+      fistName: "Надя",
+      lastName: "П.",
+      from: "от Яны У.",
       message:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, deserunt doloribus ducimus labore quisquam tempora velit! Deleniti laborum laudantium praesentium suscipit! Deserunt iure molestiae nemo ratione sunt? Asperiores, dolor, tempore.",
+        "В этот новый 2021 я хочу пожелать тебе поменьше грустить и получить больше тепла от всех людей, кто окружает. Благодарна тебе безмерно за все, что ты делаешь для нас. Надеюсь, с опытом мы будем поменьше тебе докучать со сменой паролей :D В любом случае, пусть этот год будет для тебя интереснее, полезнее и богаче на счастливые моменты!",
+    },
+    {
+      fistName: "Аня",
+      lastName: "К.",
+      from: "от Яны У.",
+      message:
+        "У меня есть даже что-то конкретное: Счастья и гармонии в новой семейной жизни! Спасибо тебе за помощь и твое время, мне всегда приятно с тобой говорить и вне работы, надеюсь, когда-нибудь в офисе эти беседы участятся. С наступающим!",
+    },
+    {
+      fistName: "Ксюша",
+      lastName: "Танакова",
+      from: "от Яны У.",
+      message:
+        "Спасибо тебе за твою энергию и поддержку! Ты на самом деле безумно внимательна к другим людям и вообще к деталям, мне это в тебе импонирует. Желаю в новом году меньше стресса, больше сил и меньше странных задач! С новым годом!",
+    },
+    {
+      fistName: "Митя",
+      lastName: "",
+      from: "от Яны У.",
+      message:
+        "Спасибо за совместную работу, помощь и знакомство с Шерри! Надеюсь, этот год подарит тебе больше здоровья и новых достижений в любых увлечениях. С новым годом!",
+    },
+    {
+      fistName: "Денис",
+      lastName: "",
+      from: "от Яны У.",
+      message:
+        "Спасибо за твои музыкальные рекомендации! Что-нибудь да нахожу для себя интересным. Пусть и у тебя в новом году будет больше открытий, и не болей!",
+    },
+    {
+      fistName: "Катя",
+      lastName: "Моргунова",
+      from: "от Яны У.",
+      message:
+        "Я ловлю все больше твою волну! И шутки сейчас воспринимаются  особеннее! Пусть в новом году будет больше и юмора, и просто приятных моментов, работа не угнетала, а настрой не подводил. Пусть все цели, что ты задумала на этот год осуществятся! С наступающим.",
+    },
+    {
+      fistName: "всем-всем",
+      lastName: "",
+      from: "от Яны У.",
+      message:
+        "Наташе Сокур, Ксюше Хоменко, Ане Богатиковой, Наде Коротченко, Ане Логвиненко, Насте Тюленевой, Ире Титовой и всей команде 2nova и Икре огромное спасибо за этот год, за поддержку, за обалденные ивенты!",
+    },
+    {
+      fistName: "Всей команде Biocad",
+      lastName: "",
+      from: "от Ани",
+      message:
+        "Ребята, огромное спасибо вам, много за что, особенно:\n" +
+        "- за то, что готовы были быстро вникать и разбираться с большой авоськой новых продуктов и новых стеков,\n" +
+        "- за вашу вовлеченность и готовность искать конкретные решения в абсолютно разных ситуациях,\n" +
+        "-  за большую отзывчивость и готовность прийти на помощь клиенту, мне и друг другу!",
+    },
+    {
+      fistName: "Наде К., Ане Б. и Кате З,",
+      lastName: "",
+      from: "от Кати Моргуновой",
+      message:
+        "Спасибо за наши спонтанные выходы в свет, когда многие даже в офис выйти боялись!) Кажется, за весь прошлый год не тусила так часто, как в те несколько недель.",
+    },
+    {
+      fistName: "Даша",
+      lastName: "",
+      from: "от Ксюши",
+      message:
+        "Спасибо тебе за этот замечательный, продуктивный, веселый год! В новом году желаю тебе: побольше высыпаться, поменьше нервничать, исполнения желаний, новых интересных увлечений и открытий!",
+    },
+    {
+      fistName: "Команда 2Nova",
+      lastName: "",
+      from: "от Кати Моргуновой",
+      message:
+        'Спасибо за корпоратив! Мне кажется, в этом году он прошел еще круче, чем в прошлом, не смотря на сорванную "Мафию" =) И он был действительно нужен всем нам, чтобы вспомнить, что мы еще вместе, и вместе мы - сила, которая порвет танцпол!)',
+    },
+    {
+      fistName: "Всем",
+      lastName: "",
+      from: "от Санты на Волге",
+      message:
+        "каждый раз, когда вы начинаете переживать о чем-то - подумайте, будет ли это важно через год.\n" +
+        "как правило, нет :)\n" +
+        "\n" +
+        "с новым годом!",
+    },
+    {
+      fistName: "Дима",
+      lastName: "Ч.",
+      from: "от Ани",
+      message:
+        "Спасибо большое за твою вовлеченность! Идеи и шаринг опыта для корпоративных систем!",
+    },
+    {
+      fistName: "Марату и Ксюше",
+      lastName: "",
+      from: "от Ани",
+      message:
+        "Спасибо за такой чудесный сюрприз в конце года!\n" +
+        "П.С. я залипаю на украшение елочки)))",
     },
   ],
+  shuffle: function (array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  },
   addItem: function (item) {
     // front
     const firstName = document.createElement("span");
@@ -274,6 +377,7 @@ const Results = {
     return lettersItem;
   },
   init: function () {
+    this.shuffle(this.data);
     const letters = document.querySelector(".letters__items");
 
     this.data.map((item) => {
